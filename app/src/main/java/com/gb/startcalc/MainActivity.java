3e815d08803f26a7ed419422ffc7fc02a3a2daf9
rodionovmax <rodionovmax90test@gmail.com>
2021-12-28 23:22:18 -0500

    added intent to start calculator app

--- app/src/main/java/com/gb/startcalc/MainActivity.java
+++ app/src/main/java/com/gb/startcalc/MainActivity.java
@@ -1,8 +1,11 @@
 package com.gb.startcalc;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button startBtn = findViewById(R.id.start_btn);
+        startBtn.setOnClickListener(view -> {
+            Uri uri = Uri.parse("example://intent");
+            Intent startCalc = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(startCalc);
+            finish();
+        });
     }
 }
